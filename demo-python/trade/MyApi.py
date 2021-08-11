b119afadf1f1a479bfd18c9a9f009d19a8c3ee7d
Azrael0507 <azrael0507@qq.com>
2021-08-11 13:49:52 +0800

    order demo
    order update

--- demo-python/trade/MyApi.py
+++ demo-python/trade/MyApi.py
@@ -14,6 +14,14 @@ AUTHCODE=configins['authinfo']['authcode']
 BROKERID=configins['baseinfo']['brokerid']
 USERID=configins['baseinfo']['userid']
 PASSWORD=configins['baseinfo']['password']
+#OrderInfo
+INSTRID=configins['orderinfo']['instrid']
+PRICETYPE=configins['orderinfo']['pricetype']
+DIR=configins['orderinfo']['dir']
+OFFSET=configins['orderinfo']['offset']
+HEDGE=configins['orderinfo']['hedge']
+PRICE=float(configins['orderinfo']['price'])
+VOLUME=int(configins['orderinfo']['volume'])
 
 
 class CTradeApi(api.CThostFtdcTraderApi):
@@ -49,6 +57,54 @@ class CTradeApi(api.CThostFtdcTraderApi):
 		loginfield.UserProductInfo="python dll"
 		self.tapi.ReqUserLogin(loginfield,0)
 		PrintSendName("ReqUserLogin")
+#结算单确认函数，每日交易前，需要首先进行结算单确认，否则报单被拒
+	def ReqSettleCf(self):
+		settleconfirm=api.CThostFtdcSettlementInfoConfirmField()
+		settleconfirm.BrokerID=BROKERID
+		settleconfirm.InvestorID=USERID
+		self.tapi.ReqSettlementInfoConfirm(settleconfirm, 0)
+		PrintSendName("ReqSettleCf")
+
+
+
+
+#报单录入函数
+	def ReqOrIn(self):
+		Orderfield = api.CThostFtdcInputOrderField()
+		Orderfield.BrokerID=BROKERID
+		Orderfield.InvestorID=USERID
+		Orderfield.InstrumentID=INSTRID
+		Orderfield.OrderRef="001"
+		Orderfield.OrderPriceType=PRICETYPE
+		Orderfield.Direction=DIR
+		Orderfield.CombOffsetFlag=OFFSET
+		Orderfield.CombHedgeFlag=HEDGE
+		Orderfield.LimitPrice=PRICE
+		Orderfield.VolumeTotalOriginal=VOLUME
+		Orderfield.TimeCondition=api.THOST_FTDC_TC_GFD
+		Orderfield.VolumeCondition=api.THOST_FTDC_VC_AV
+		Orderfield.ContingentCondition=api.THOST_FTDC_CC_Immediately
+		Orderfield.ForceCloseReason=api.THOST_FTDC_FCC_NotForceClose
+		Orderfield.IsAutoSuspend=0
+		Orderfield.IsSwapOrder=0
+		Orderfield.UserForceClose=0
+		self.tapi.ReqOrderInsert(Orderfield,0)
+		PrintSendName("ReqOrderInsert")
+
+#报单撤销函数
+	def ReqOrDel(self, ExchangeID, OrderSysID):
+		pDelOrder=api.CThostFtdcInputOrderActionField()
+		pDelOrder.BrokerID=BROKERID
+		pDelOrder.InvestorID=USERID
+		pDelOrder.UserID=USERID
+		pDelOrder.ExchangeID=ExchangeID
+		pDelOrder.ActionFlag=api.THOST_FTDC_AF_Delete
+		pDelOrder.OrderSysID=OrderSysID
+		self.tapi.ReqOrderAction(pDelOrder, 0)
+		PrintSendName("ReqOrderAction")
+
+
+	
 
 def PrintSendName(sendname):
 	print("send", sendname, "ok")
--- demo-python/trade/MySpi.py
+++ demo-python/trade/MySpi.py
@@ -29,6 +29,35 @@ class CTradeSpi(api.CThostFtdcTraderSpi):
 	def OnRspUserLogin(self, pRspUserLogin: 'CThostFtdcRspUserLoginField', pRspInfo: 'CThostFtdcRspInfoField', nRequestID: 'int', bIsLast: 'bool') -> "void":
 		PrintReName("OnRspUserLogin")
 		print ("ErrorID=", pRspInfo.ErrorID, "ErrorMsg=", pRspInfo.ErrorMsg)
+		self.myapi.ReqSettleCf()
+
+#结算单确认回调函数，确认完毕后进入报单
+	def OnRspSettlementInfoConfirm(self, pSettlementInfoConfirm: 'CThostFtdcSettlementInfoConfirmField', pRspInfo: 'CThostFtdcRspInfoField', nRequestID: 'int', bIsLast: 'bool') -> "void":
+		PrintReName("OnRspSettlementInfoConfirm")
+		print ("ErrorID=", pRspInfo.ErrorID, "ErrorMsg=", pRspInfo.ErrorMsg)
+		self.myapi.ReqOrIn()
+
+#报单插入对应的4组不同的回调函数
+	def OnRspOrderInsert(self, pInputOrder: 'CThostFtdcInputOrderField', pRspInfo: 'CThostFtdcRspInfoField', nRequestID: 'int', bIsLast: 'bool') -> "void":
+		PrintReName("OnRspOrderInsert")
+		print ("ErrorID=", pRspInfo.ErrorID, "ErrorMsg=", pRspInfo.ErrorMsg)
+	
+
+	def OnErrRtnOrderInsert(self, pInputOrder: 'CThostFtdcInputOrderField', pRspInfo: 'CThostFtdcRspInfoField') -> "void":
+		PrintReName("OnErrRtnOrderInsert")
+		print ("ErrorMsg=", pRspInfo.ErrorMsg)
+	
+	def OnRtnOrder(self, pOrder: 'CThostFtdcOrderField' ) -> "void":
+		PrintReName("OnRtnOrder")
+		print ("investorid=", pOrder.InvestorID , "orderstatus=", pOrder.OrderStatus, "OrderSysID", pOrder.OrderSysID)
+		print ("investorid=", pOrder.InvestorID , "ExchangeID=", pOrder.ExchangeID, "OrderSysID", pOrder.OrderSysID)
+		exchangeid = pOrder.ExchangeID
+		sysid = pOrder.OrderSysID
+		self.myapi.ReqOrDel(exchangeid, sysid)
+
+	def OnRtnTrade(self, pTrade: 'CThostFtdcTradeField' ) -> "void":
+		PrintReName("OnRtnTrade")
+		print ("investorid=", pTrade.InvestorID , "TradeID=", pTrade.TradeID, "OrderSysID", pTrade.OrderSysID)
 		
 
 #静态处理方法内容
